ea4baa93c868bf60ded485ca74499c9a1efe949d
PrudenceGikundi <prudencekendi101@gmail.com>
2023-11-27 19:06:57 +0300

    first commit

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+word= "Holberton"
+word_first_3= "Hol"
+word_last_2="on"
+middle_word="olbert"
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
+print("word: {}".format(word))
