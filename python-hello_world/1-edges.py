8cc3df92918f0816b30908f1f3998c7cfbdc59fd
PrudenceGikundi <prudencekendi101@gmail.com>
2023-11-28 13:34:53 +0300

    updating variables

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,4 +5,9 @@ middle_word="olberto"
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
-
+newword_first_3 = word_first_3.replace("Hol", "sch")
+newword_last_2 = word_last_2.replace("on", "ol")
+newmiddle_word = middle_word.replace("olberto", "choo")
+print(newword_first_3)
+print(newword_last_2)
+print(newmiddle_word)
