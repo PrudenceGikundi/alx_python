7a3e77b1a105248937a61dabac446297d07a1455
PrudenceGikundi <prudencekendi101@gmail.com>
2023-11-28 23:01:13 +0300

    commit

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,5 @@
 word= "Holberton"
 print("First 3 letters: {}".format(word[0:3]))
-print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]))
-word="school"
-print("First 3 letters: {}".format(word[0:3]))
 print("Last 2 letters: {}".format(word[-2:]))
-print("Middle word: {}".format(word[1:5]))
+print("Middle word: {}".format(word[1:-1]))
+
