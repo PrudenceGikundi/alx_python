83f51b4eb7c946c45a95e8f38be3ee848a73a094
PrudenceGikundi <prudencekendi101@gmail.com>
2023-11-28 13:42:36 +0300

    update

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 word= "Holberton"
-word_first_3= "Hol"
-word_last_2="on"
-middle_word="olberto"
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
+print("First 3 letters: {}".format("Hol"))
+print("Last 2 letters: {}".format("on"))
+print("Middle word: {}".format("olberto"))
 print(word)
+print("First 3 letters: {}".format("sch"))
+print("Last 2 letters: {}".format("ol"))
+print("Middle word: {}".format("choo"))
