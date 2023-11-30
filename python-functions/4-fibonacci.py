a7d55430a97015b351a6d856789315ce2daa46dc
PrudenceGikundi <prudencekendi101@gmail.com>
2023-11-30 11:59:33 +0300

    sequence

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,12 +1,13 @@
 def fibonacci_sequence(n):
-    if n <= 0:
-        return "Incorrect input"
-    elif n == 1:
-        return 0
-    elif n == 2:
-        return 1
-    else:
-        a, b = 0, 1
-        for _ in range(2, n):
-            a, b = b, a + b
-        return b
+    if n < 0:
+        return []
+
+    result = []
+    a = 0
+    b = 1
+
+    for i in range(n):
+        result.append(a)
+        a, b = b, a + b
+
+    return result
