51446cc00a1781bb050d5d1f3783852769e992d5
PrudenceGikundi <prudencekendi101@gmail.com>
2023-11-30 12:23:55 +0300

    prime commit

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,6 +1,7 @@
 def is_prime(num):
-  if num%num==0 and num%1==0:
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
     return True
-  else:
-    return False
-  
