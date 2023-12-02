824e7719797202e77095014019e549a9d367c506
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-02 12:16:07 +0300

    add commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,11 @@
+def add(a, b):
+    return a + b
+
+def main():
+    a = 1
+    b = 2
+    add_result = add(a, b)
+    print("The result is: {}".format(add_result))
+
 if __name__ == "__main__":
-    
- def add(a, b):
-    a=1
-    b=2
-    print ("{}+{}={}".format(a,b,(a+b)))
-
-
+    main()
