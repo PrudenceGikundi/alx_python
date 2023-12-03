f21ba5bbc6241cc0c23192ed6abba9b476bee431
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-03 13:16:00 +0300

    add commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,11 @@
-from .add_0 import add
-a=1
-b=2
-print ("{}+{}= {}". format(a,b,add(a+b)))
+from add_0 import add
+
+def main():
+    a=1
+    b=2
+    sum=a+b
+    print ("{}+{}= {}". format(a,b,sum))
+
+if __name__== "__main__":
+  main()
+  
