5c1e75db28112967db6990540efed8a63ff6fdf1
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-03 13:50:38 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 from add_0 import add
-
+from add_0 import fakeAdd
 def main():
     a = 1
     b = 2
     sum= add(a, b)
     print ("{}+{}={}".format(a,b,sum))
+    diff= fakeAdd(a, b)
+    print ("{}+{}={}".format(a,b,diff))
 
 if __name__== "__main__":
   main()
