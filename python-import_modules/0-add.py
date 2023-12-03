6ea7d70aa33aa6e585041293918240893d12826f
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-03 12:45:39 +0300

    commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,4 @@
-def add(a, b):
-    return a + b
-
-def main():
-    a = 1
-    b = 2
-    add_result = add(a, b)
-    print("The result is: {}".format(add_result))
-
-if __name__ == "__main__":
-    main()
+from .add_0 import add
+a=1
+b=2
+print ("{}+{}= {}". format(a,b,add(a+b)))
