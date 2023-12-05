05d300a4a2b36f36f2ebe2ceb2fe88443847c7ea
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-05 22:24:15 +0300

    Ommit

---
+++ python-import_modules/stringmanzewd.py
@@ -0,0 +1,12 @@
+def no_c(my_string):
+  #empty variable new_string to store our output
+  new_string= "" 
+  #iterate through each character
+  for char in my_string:
+    # Check if the current character is NOT equal to'c' and'C' using the in operator
+    if char not in ["c","C"]:
+        # If true, it is not c or C, so append (+=) the char to new_string
+      new_string += char
+  return new_string
+print(no_c("I love Coconut and Corn"))
+  #wrong directory manze..supposed to be in the datastructures one
