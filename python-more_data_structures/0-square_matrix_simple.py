3c231deda256e69c034d1351893ac14642fd1554
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-07 14:54:33 +0300

    matrix

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,11 @@
 def square_matrix_simple(matrix=[]):
-    result = [] 
+    result_matrix = [] 
     for row in matrix:
         square_row = []  
         for element in row:
             square_row.append(element ** 2)  
-        result.append(square_row) 
-    return result  
+        result_matrix.append(square_row) 
+    return result_matrix  
 
 if __name__== "__main__":
   matrix = [
