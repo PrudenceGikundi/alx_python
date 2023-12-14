a5f464ce981b7f0678ea25cc8f1a32199157ab38
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-14 09:44:51 +0300

    changes

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,8 +1,5 @@
-'''create a class Similar to hold our funtion'''
-class Similar():
-  pass
-  '''define a function that '''
-  def is_instance_of_class(obj, cls):
+'''create a class Similar to hold our funtion define a function that '''
+def is_instance_of_class(obj, cls):
     '''cls stands for class,obj object
        In this function, obj is the object we want to check, and cls is the class we want to check against. 
        The function first checks if obj is an instance of cls using isinstance(). If it is,
@@ -11,5 +8,5 @@ class Similar():
        indicating that obj is exactly an instance of cls. else, the function returns False
     '''
     return isinstance(obj, cls) and type(obj) == cls
-  '''returns true or false'''
+    '''returns true or false'''
 
