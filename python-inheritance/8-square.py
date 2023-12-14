ee0a53c3ef32a99fc5316b192d3be00051c4e0d1
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-14 14:27:14 +0300

    square

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,13 @@
+'''import the 7-base_geometry module'''
+Rectangle = __import__('7-rectangle').Rectangle
+'''create a square class that inherits rectngle'''
+class Square(Rectangle):
+   def __init__(self, size):
+      self.__size = size
+      self.integer_validator("size", size)
+
+   def __str__(self):
+      '''returns a string representation of our square'''
+      return "[Square] {}/{} {:d}/{:d}".format(self.__size, self.__size) 
+   def area(self):
+      return self.__size ** 2
