20a42b473226cb9b508e21e082b3f918612b5011
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-14 06:08:57 +0300

    SQUARE

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 '''create a class'''
-class square:
-  '''CREATES A CLASSS SQUARE AND INITIALIZIES A SQUARE OBJECT
+class Square:
+  '''CREATES A CLASS SQUARE AND INITIALIZIES A SQUARE OBJECT
     WITH A SPECIFIC SIZE'''
   def __init__(self, size):
    '''__init__ is the constructor method to initialize object size and sets the value to __size'''
