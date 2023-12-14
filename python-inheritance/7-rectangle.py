c1b112b89db5636aec07c96a26bc581f30a5d968
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-14 15:09:34 +0300

    sortingdir

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -17,4 +17,12 @@ class Rectangle(BaseGeometry):
     '''string to represent our rectangle '''
     def __str__(self):
         return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
-  
+    def __dir__(self):
+     return sorted(
+        ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', 
+         '__eq__', '__format__', '__ge__', '__getattribute__', 
+         '__gt__', '__hash__', '__init__', '__le__', '__lt__', '__module__',
+           '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', 
+           '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__',
+             'area', 'integer_validator']
+     )
