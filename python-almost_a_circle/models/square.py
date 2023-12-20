c8102d93e9e7434bd7222e9ca4653ecff0b24472
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-20 13:31:26 +0300

    square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,4 +3,9 @@ from models.rectangle import Rectangle
 '''create a class '''
 class Square(Rectangle):
   '''a class squre that inherits from Rectangle class'''
-  pass
+  def __init__(self,size,x=0,y=0,id=None):
+    self.size= size
+    super().__init__(size,size,x,y,id)
+  def __str__(self):
+   return  f"[Square] ({self.id}){self.x}/{self.y} - {self.size}"
+    
