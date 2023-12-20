2359d9388df5c9be0ea543bfdecd0f75ecb35252
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-20 13:33:52 +0300

    spaces

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -7,5 +7,5 @@ class Square(Rectangle):
     self.size= size
     super().__init__(size,size,x,y,id)
   def __str__(self):
-   return  f"[Square] ({self.id}){self.x}/{self.y} - {self.size}"
+   return  f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
     
