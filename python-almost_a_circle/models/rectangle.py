6f1ce6ad11beb5bfc03ab5bd042d90a1b9711e95
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-20 11:31:19 +0300

    areacommit

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -56,6 +56,12 @@ class Rectangle(Base):
     if value < 0:
       raise ValueError("y must be >= 0")
     self.__y = value
+
+  def area(self):
+    '''defining the area of a rectangle'''
+    area = self.__width * self.__height
+    return area
+
     
 
     
