09f0daa8d7404f3f0600f1bcb7a74ec1545899cb
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-20 22:38:32 +0300

    response

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,4 +3,6 @@ import requests
 
 '''takes the url and request it using the get method'''
 url = "https://alu-intranet.hbtn.io/status"
-requests.get(url)
+
+response = requests.get(url)
+print(response.status_code)
