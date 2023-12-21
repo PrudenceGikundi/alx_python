321ae98e5ce29215de0ea6d18b8a727a08836dea
PrudenceGikundi <prudencekendi101@gmail.com>
2023-12-21 10:47:51 +0300

    commit

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -10,17 +10,41 @@ You are not allowed to import packages other than requests and sys
 You don’t need to check arguments passed to the script (number or type)'''
 
 '''import request and sys '''
-import sys
+# import sys
+# import requests
+# username= sys.argv[1]
+# password=sys.argv[2]
+# url = "https://github.com/PrudenceGikundi"
+
+# response = requests.get (url, auth=(username, password))
+
+# if response.status_code ==200:
+#   user_id = response.json()["id"]
+#   print(f"user ID: {user_id}")
+# else:
+#   print("27247273")
+# try:
+#    check_json = response.json()
+#    if not check_json:
+#     print ("No result")
+#    else:
+#      id =check_json.get("id")
+#      name = check_json.get("name")
+#      print(f"[{id}] {name}")
+# except ValueError:
+#   print("Not a valid JSON")
 import requests
-username= sys.argv[1]
-password=sys.argv[2]
-url = "https://github.com/PrudenceGikundip"
+from requests.auth import HTTPBasicAuth
+import sys
 
-response = requests.get (url, auth=(username, password))
+username = sys.argv[1]
+access_token = sys.argv[2]
 
-if response.status_code ==200:
-  user_id = response.json()["id"]
-  print(f"user ID: {user_id}")
+response = requests.get("https://api.github.com/user", 
+                        auth=HTTPBasicAuth(username, access_token))
+
+if response.status_code == 200:
+    user_data = response.json()
+    print(user_data['id'])
 else:
-  print("Failed to fetch user ID")
-
+    print("Invalid credentials")
