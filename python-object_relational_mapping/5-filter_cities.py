3d1361f4ddb61fc5285a3fb11d96c2c3c0d404b9
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-17 23:29:23 +0300

    lent

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,12 +14,15 @@ if __name__ == '__main__':
                FROM cities
                INNER JOIN states ON
                cities.state_id=states.id
-                WHERE state.name LIKE BINARY %s
-                ORDER BY cities.id
+               WHERE state.name = %s
+               ORDER BY cities.id
                 """
     cursor.execute(query, (argv[4],))
     rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    for i, row in enumerate(rows):
+        if i==len(row)-1:
+            print("".join(row[0]),end=' ')
+        else:
+            print("".join(row[0]),end=',')
     cursor.close()
     conn.close()
