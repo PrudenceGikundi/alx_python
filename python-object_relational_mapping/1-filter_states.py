9ce990d13e6d5ccd6cd9771a574ce6b7e4ca3633
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-17 16:10:59 +0300

    PUSH

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,6 +10,8 @@ if __name__ == '__main__':  # Note the double underscores in __name_
     conn = MySQLdb.connect(host='localhost', passwd=MySql_pass, port=3306, user=MySql_username, db=db_name)
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM states WHERE BINARY LEFT(name, 1) = "N" ORDER BY id ASC')
+    '''This query will select the name column from the table where the first letter of the name is a capital 'N'.
+      The BINARY keyword is used to perform a case-sensitive comparison.'''
     rows = cursor.fetchall()
 
     for row in rows:  # Iterate through each row
---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    mysql_username = argv[1]
+    mysql_passwd = argv[2]
+    db_name = argv[3]
+    statename_searched = argv[4]
+
+    conn = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_passwd, port=3306, db=db_name)
+    cursor = conn.cursor()
+    query = "SELECT * FROM states WHERE name = %s"
+    cursor.execute(query, (statename_searched,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    conn.close()
