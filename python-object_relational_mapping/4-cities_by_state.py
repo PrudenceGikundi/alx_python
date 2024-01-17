cecadf77ec82a109bdf8c58cb7149923aed76a1a
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-17 22:51:22 +0300

    com5

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
                INNER JOIN states ON
                cities.state_id=states.id
                 ORDER BY cities.id"""
-    cursor.execute(query, (argv[4],))
+    cursor.execute(query)
     rows = cursor.fetchall()
     for row in rows:
         print(row)
