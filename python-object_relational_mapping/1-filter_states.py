0f4b3fd03bc378a732d7f6e52b833a28e88bf205
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-17 15:11:28 +0300

    filter

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,19 @@
 import MySQLdb
 from sys import argv
 
-if __name__== '__main__':
+if __name__ == '__main__':  # Note the double underscores in __name_
 
-   MySql_username=argv[1]
-   MySql_pass= argv[2]
-   db_name=argv[3]
+    MySql_username = argv[1]
+    MySql_pass = argv[2]
+    db_name = argv[3]
 
-   conn= MySQLdb.connect(host='localhost',passwd=MySql_username,port=3306,user=MySql_username,db=db_name)
-   cursor=conn.cursor()
-   cursor.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
-   rows=cursor.fetchall()
-   for row in rows:
-      print(rows)
-   cursor.close()
-   conn.close()
+    conn = MySQLdb.connect(host='localhost', passwd=MySql_pass, port=3306, user=MySql_username, db=db_name)
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM states WHERE name LIKE "N%" ORDER BY id ASC')
+    rows = cursor.fetchall()
 
+    for row in rows:  # Iterate through each row
+        print(row)  # Print the individual row tuple
+
+    cursor.close()
+    conn.close()
