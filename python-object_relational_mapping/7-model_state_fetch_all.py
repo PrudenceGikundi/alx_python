c3b187b037be4b61aa56560713f5648ab7864a19
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-18 21:54:52 +0300

    push2

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,21 +2,29 @@
 ''' a script that lists all State objects from the database hbtn_0e_6_usa
      Your script should connect to a MySQL server running on localhost at port 3306
 '''
-from sys import argv
-import sqlalchemy
-from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
-# Create the declarative base
-Base = declarative_base()
+if __name__ == '__main__':
+    # Create a connection to the database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-# Define the State classs
-class State(Base):
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
-    name = Column(String(128), nullable=False)
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-# Connect to the MySQL server
-engine = create_engine('mysql://user:password@localhost:3306/db_name')
-Base.metadata.create_all(engine)
+    # Query the database for all State objects
+    states = session.query(State).order_by(State.id).all()
+
+    # Print the results
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
