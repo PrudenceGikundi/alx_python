c5a4fc08c8fdd264f67b319814bee6a8165b0609
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-23 17:20:28 +0300

    com4

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -20,7 +20,7 @@ def c(text):
     return f'C {text}'
 @app.route("/python/<text>",strict_slashes=False)
 def python(text):
-   text=text.replace('_',' ')
+   text=text.replace('_','  ')
    return f'python{text}'
 
 if __name__=="__main__":
