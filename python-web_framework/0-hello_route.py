65b7f4d0ca82228bd4705e4eab0c0ff7c1280354
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-23 16:19:15 +0300

    commt6

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -2,6 +2,8 @@
 from flask import Flask
 '''creating an instance of the class '''
 app= Flask(__name__)
-@app.route("")
+@app.route("/",strict_slashes=False)
 def hello_world():
   return "Hello HBNB!"
+if __name__=="__main__":
+  app.run(host='0.0.0.0',port=5000, debug=True)
