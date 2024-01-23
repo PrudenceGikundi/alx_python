354f3ed9b377a4e292360e11fe0f84012abbda20
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-23 15:55:05 +0300

    commit3

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -3,6 +3,7 @@
    must use the option strict_slashes=False in your route definition
 '''
 from flask import Flask
+'''creating an instance of the class '''
 app= Flask(__name__)
 @app.route("")
 def hello_world():
