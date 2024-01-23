dc4166cd034e4402ac0e07358a821fc197c29b52
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-23 15:56:14 +0300

    commt4

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -2,6 +2,7 @@
    Your web application must be listening on 0.0.0.0, port 5000
    must use the option strict_slashes=False in your route definition
 '''
+#module is documented
 from flask import Flask
 '''creating an instance of the class '''
 app= Flask(__name__)
