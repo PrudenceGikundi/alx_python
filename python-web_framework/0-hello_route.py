e65e4e5af8c91410d22bc7bc7940eba0f099040d
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-23 15:57:16 +0300

    doc5

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,8 +1,4 @@
-'''a script that starts a Flask web application
-   Your web application must be listening on 0.0.0.0, port 5000
-   must use the option strict_slashes=False in your route definition
-'''
-#module is documented
+'''a script that starts a Flask web application'''
 from flask import Flask
 '''creating an instance of the class '''
 app= Flask(__name__)
