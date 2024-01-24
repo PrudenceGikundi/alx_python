35117f13d4af0975f3fc57fe07a21db73143db76
PrudenceGikundi <prudencekendi101@gmail.com>
2024-01-24 16:20:16 +0300

    push

---
+++ python-web_framework/6-number_odd_or_even.py
@@ -0,0 +1,39 @@
+'''a script that starts a Flask web application'''
+from flask import Flask, render_template, redirect, url_for 
+from markupsafe import escape
+'''creating an instance of the class '''
+app= Flask(__name__)
+@app.route("/",strict_slashes=False)
+# The strict_slashes=False option is used to allow both '/path' and '/path/' to work the same way
+
+#A view function ,hello_world is defined to handle the root route
+def hello_world():
+
+  return "Hello HBNB!"
+
+@app.route("/hbnb",strict_slashes=False)
+def hello():
+  return "HBNB"
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    text = text.replace('_', ' ')
+    return f'C {text}'
+@app.route("/python/<text>",strict_slashes=False)#,defaults={'text':'is cool'}
+def Python(text):
+   text=text.replace('_', ' ')
+   
+   return f'Python {text}'
+
+'''Routing to /number_template'''
+@app.route("/number_template/<int:n>")
+def temp(n):
+    return render_template("5-number.html", n=n)
+
+@app.route("/number_odd_or_even/<int:n>")
+def odd_even(n):
+    return render_template('6-number_odd_or_even.html', n=n, output="even" if n % 2==0 else "odd")
+    
+
+
+if __name__=="__main__":
+  app.run(host='0.0.0.0',port=5000, debug=True)
