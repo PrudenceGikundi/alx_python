93664eda6a19586a395c8e3cce9a46d08c4b35a9
PrudenceGikundi <prudencekendi101@gmail.com>
2024-02-27 00:57:10 +0300

    COM

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -23,7 +23,7 @@ completed_task=len(completed)
 
 
 # print('completed')
-print(f"employee {name} is done with tasks ({completed_task}/{total_tasks}):")
+print(f"Employee {name} is done with tasks ({completed_task}/{total_tasks}):")
 
 for i in completed:
   print(f"/t{i}")
