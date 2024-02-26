4287649928e9551b3d45a9783f4e37b068d8c4a8
PrudenceGikundi <prudencekendi101@gmail.com>
2024-02-27 00:53:48 +0300

    com2

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,13 +1,29 @@
 import requests
 import sys
+from urllib import response
 
 url="https://jsonplaceholder.typicode.com/users"
 user_id=sys.argv[1]
-endpoint= url + "/{}".format(user_id) +"/todos"
+endpoint1= url + "/{}".format(user_id)
+endpoint2= url + "/{}".format(user_id) +"/todos"
 # print(endpoint)
 
-response=requests.get(url=endpoint)
-print(response.json())
+response1=requests.get(url=endpoint1)
+response2=requests.get(url=endpoint2)
+# print(response1.json())
+# print(response2.json())
+
+data1=response1.json()
+data2=response2.json()
+
+name=data1["name"]
+total_tasks=len(data2)
+completed = [task['title'] for task in data2 if task['completed']]
+completed_task=len(completed)
 
 
+# print('completed')
+print(f"employee {name} is done with tasks ({completed_task}/{total_tasks}):")
 
+for i in completed:
+  print(f"/t{i}")
