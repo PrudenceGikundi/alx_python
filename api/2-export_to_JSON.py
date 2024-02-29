0b34b44b48af4de9e228e96d9b5f8d6d968b710d
PrudenceGikundi <prudencekendi101@gmail.com>
2024-02-29 13:08:06 +0300

    comw

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,29 +1,44 @@
+"""Import the needed modules in alphabetical order"""
+import json
 import requests
 import sys
-from urllib import response
 
-url="https://jsonplaceholder.typicode.com/users/json"
-user_id=sys.argv[1]
-endpoint1= url + "/{}".format(user_id)
-endpoint2= url + "/{}".format(user_id) +"/todos"
-# print(endpoint)
+"""This function will take an employee's id as an argument and return a json file with the required details"""
+def get_employee_info(employee_id):
+    """Fetch the employee details from the given url by appending the employee_id and convert the data to json"""
+    employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    employee_response = requests.get(employee_url)
+    employee_data = employee_response.json()
 
-response1=requests.get(url=endpoint1)
-response2=requests.get(url=endpoint2)
-# print(response1.json())
-# print(response2.json())
+    """fetch the employee's todo by appending the todo route to the url"""
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
 
-data1=response1.json()
-data2=response2.json()
+    """create a json file for the employee with required format"""
+    json_file_path = f"{employee_id}.json"
+    json_data = {
+        f"{employee_id}": [
+            {
+                "task": task['title'],
+                "completed": task['completed'],
+                "username": employee_data['username']
+            }
+            for task in todos_data
+        ]
+    }
 
-name=data1["name"]
-total_tasks=len(data2)
-completed = [task['title'] for task in data2 if task['completed']]
-completed_task=len(completed)
+    """Write the json file with the required details"""
+    with open(json_file_path, 'w') as json_file:
+        json.dump(json_data, json_file, indent=2)
+"""Obtain the employees details from the command line using the sys module"""
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py employee_id.")
+        sys.exit(1)
 
+    """Get the employee id from the second argument"""
+    employee_id = int(sys.argv[1])
 
-# print('completed')
-print(f"Employee {name} is done with tasks({completed_task}/{total_tasks}):")
-
-for i in completed:
-  print(f"\t {i}")
+    """Call the function with the provided id"""
+    get_employee_info(employee_id)
